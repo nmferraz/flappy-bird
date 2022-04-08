dd2713a3fc157623042c11bba72d18490a041da3
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-04-08 15:05:56 +0100

    cleaning the code

--- states/TitleScreenState.lua
+++ states/TitleScreenState.lua
@@ -1,23 +1,12 @@
---[[
-    TitleScreenState Class
-    Author: Colton Ogden
-    cogden@cs50.harvard.edu
-
-    The TitleScreenState is the starting screen of the game, shown on startup. It should
-    display "Press Enter" and also our highest score.
-]]
-
 TitleScreenState = Class{__includes = BaseState}
 
 function TitleScreenState:update(dt)
-    -- transition to countdown when enter/return are pressed
     if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
         gStateMachine:change('countdown')
     end
 end
 
 function TitleScreenState:render()
-    -- simple UI code
     love.graphics.setFont(flappyFont)
     love.graphics.printf('Fifty Bird', 0, 64, VIRTUAL_WIDTH, 'center')
 
